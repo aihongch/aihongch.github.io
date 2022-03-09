aec09e1e3073fdce3e9b320556059b8329a2e8ce
Aihong Ella Chen <chenaiho@msu.edu>
2022-03-08 18:58:59 -0800

    HW2 update

--- EllaWillGuess/app.py
+++ EllaWillGuess/app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+       age = request.form['age']
+       weight = request.form['weight']
+       return render_template('Index.html', result=age)
+
+    return render_template('Index.html')
+
+
+if __name__ == '__main__':
+   app.run()
